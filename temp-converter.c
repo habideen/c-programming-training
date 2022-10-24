93c41f4a8ec07d978bf5245630a292251aab9c8a
Habideen Ibrahim <habideenibrahim@gmail.com>
2022-10-24 16:53:24 +0100

    Fahrenheit to celsius converter modification

--- temp-converter.c
+++ temp-converter.c
@@ -7,18 +7,20 @@
  */
 int main(void)
 {
-	int celsius, fahr;
+	float celsius, fahr;
 	int lower, upper, step;
 
 	lower = 0;	/* Lower limit of temperature */
 	upper = 300;	/* Upper limit  */
 	step = 20;	/* step size  */
 
+	printf("%7s%15s\n", "Celsius", "Fahrenheit");
+
 	fahr = lower;
 	while (fahr <= upper)
 	{
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = 5.0 / 9.0 * (fahr - 32);
+		printf("%7.0f\t%14.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 
