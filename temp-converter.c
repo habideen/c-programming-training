faf5d2420459fcded5cdf5b3bf2ae06b440e027b
Habideen Ibrahim <habideenibrahim@gmail.com>
2022-10-24 16:35:51 +0100

    Fahrenheit to celsius converter

--- temp-converter.c
+++ temp-converter.c
@@ -3,24 +3,24 @@
 /**
  * main - Fahrenheit to Celcius calculator
  *
- * return - 0 on success
+ * Return: 0 on success
  */
-int main()
+int main(void)
 {
 	int celsius, fahr;
 	int lower, upper, step;
 
 	lower = 0;	/* Lower limit of temperature */
-	upper = 300; 	/* Upper limit  */
+	upper = 300;	/* Upper limit  */
 	step = 20;	/* step size  */
 
 	fahr = lower;
-	while(fahr <= upper)
+	while (fahr <= upper)
 	{
 		celsius = 5 * (fahr - 32) / 9;
 		printf("%d\t%d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 
-	return 0;
+	return (0);
 }
